7f942c596ec5368ae3514eead41dbeb444b69e92
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-11-07 19:36:09 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,12 +6,10 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
   name_hash.each do |k, v|
-    if lowest_value == nil 
+    if lowest_value == nil
       lowest_value = v
       lowest_key = k
     end
   end
 lowest_key
 end
-
-
